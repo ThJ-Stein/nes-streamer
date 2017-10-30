b6f1bc7fbc5e689000182bdbc787d343ac239110
Alex <ale.coroiu@gmail.com>
2017-10-30 16:56:08 +0100

    Web page Now the settings are sent when pressing start (including key and channel id -used for the iframe link). The settings are sent as a json through a post request. There are default values for all settings. Also, the file for streaming is now supposed to be in raspbian. The applciation has to be started in flask so it can properly look for the streamingFile. Everything still looks ugly.

--- raspbian/flask/rest.py
+++ raspbian/flask/rest.py
@@ -1,57 +1,50 @@
 #!bin/python
-from flask import Flask, jsonify, request
+from flask import Flask, json, request
 import subprocess
 import signal
 
-app = Flask(__name__, static_url_path='/home/thomas/Documents/nes-streamer/flask/static')
+app = Flask(__name__, static_url_path='/home/nes-streamer/raspbian/flask')
 
-#settings
-vbr = "2500k"
-fps = "30"
-qual = "medium"
 youtube_url = "rtmp://a.rtmp.youtube.com/live2"
 # THOMAS DEFIANTLY DOESTNS PRIRATE source = "\"/home/thomas/Shared Disk/Shared Disk/Downloads/_Torrents/Rick and Morty Season 1 [1080p] [HEVC]/[pseudo] Rick and Morty S01E01 - Pilot [1080p] [h.265].mkv\""
-source = "/home/pi/nes-streamer/raspbian/big_buck_bunny_720p_stereo.ogg"
-key = "4u3d-d39c-cp1c-bruj" 
+source = "../streamFile"
 
 process = None
 
 @app.route('/')
 def index():
-	return app.send_static_file('home.html')
+        return app.send_static_file('home.html')
 
 @app.route('/start', methods=['POST'])
 def api_start_stream():
-	print('starting stream')
-	start_stream()
-	return "yay"
+        global settings;
+        print('starting stream')
+        settings = request.get_json()
+        print('got json')
+        start_stream()
+        return "yay"
 
 @app.route('/stop', methods=['POST'])
 def api_stop_stream():
-	print('stopping stream')
-	stop_stream()
-	return "aww"
-
-@app.route('/settings', methods=['PUT'])
-def api_post_settings():
-	print 'settings'
-	print request.get_json()
-	return 0
+        print('stopping stream')
+        stop_stream()
+        return "aww"
 
 #the methods to start the stream
 def start_stream():
-	global process
-	command = "exec ffmpeg -i %s -deinterlace -vcodec libx264 -pix_fmt yuv420p" \
-	" -preset %s -r %s -g $((%s * 2)) -b:v %s -acodec libmp3lame -ar" \
-	" 44100 -threads 6 -qscale 3 -b:a 712000 -bufsize 512k -f flv" \
-	" \"%s/%s\"" % (source, qual, fps, fps, vbr, youtube_url, key)
-	process = subprocess.Popen(command, shell=True)
+        global process
+        global settings
+        command = "exec ffmpeg -i %s -deinterlace -vcodec libx264 -pix_fmt yuv420p" \
+        " -preset %s -r %s -g $((%s * 2)) -b:v %s -acodec libmp3lame -ar" \
+        " 44100 -threads 6 -qscale 3 -b:a 712000 -bufsize 512k -f flv" \
+        " \"%s/%s\"" % (source,settings["quality"], settings["fps"], settings["fps"], settings["bitrate"], youtube_url, settings["key"])
+        process = subprocess.Popen(command, shell=True)
 
 def stop_stream():
-	global process
-	print(process.pid)
-	process.kill()
+        global process
+        print(process.pid)
+        process.kill()
 
 
 if __name__ == '__main__':
-	app.run(debug=True,  host='0.0.0.0')
+        app.run(debug=True,  host='0.0.0.0')
